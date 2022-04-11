4e852fce739d01fdba073e3ddb240f177fa7e3ba
shivprajapat <shivkumarprajapat232@gmail.com>
2022-04-11 09:31:04 +0530

    Basic Client-Server Communication Added

--- server/index.js
+++ server/index.js
@@ -2,7 +2,9 @@ const WebSocket = require('ws');
 const wss = new WebSocket.Server({
     port: 9876
 },function () {
-    console.log('Say hi to websocket on port http://localhost:9876');
+    wss.on('connection',(ws)=>{
+     ws.send('Hello Server',ws)
+    })
 
 })
 
