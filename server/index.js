afa0b2a60ef513cfe8df68bb270ec1efe0086ee6
shivprajapat <shivkumarprajapat232@gmail.com>
2022-04-11 09:33:03 +0530

    onMessage Added

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
-const WebSocket = require('ws');
+const WebSocket = require('ws')
+
 const wss = new WebSocket.Server({
     port: 9876
-},function () {
-    wss.on('connection',(ws)=>{
-     ws.send('Hello Server',ws)
+}, function () {
+    wss.on('connection', (ws) => {
+        ws.on('message', function (data) {
+            console.log('WebSocket 🔥 ready on port http://localhost:5500');
+        })
     })
-
 })
+
 
 console.log(wss);
