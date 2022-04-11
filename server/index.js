2d65d080529db1026714d2830ec1295998458714
shivprajapat <shivkumarprajapat232@gmail.com>
2022-04-11 16:27:08 +0530

    Express Server Created

--- server/index.js
+++ server/index.js
@@ -1,7 +1,14 @@
 const WebSocket = require('ws')
+const express = require('express');
+
+const app = express()
+const path = require('path')
+app.use('/', express.static(path.resolve(__dirname, '../clients')));
+
+const server =app.listen(9876)
 
 const wss = new WebSocket.Server({
-    port: 9876
+    server
 })
 wss.on('connection', function (ws) {
     ws.on('message', function (data) {
